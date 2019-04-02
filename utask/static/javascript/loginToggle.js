818646f7f0d52de40186b4993f5e703d35abfdd9
Robert <robertnote23@gmail.com>
2019-04-02 09:36:17 +0200

    try to solve settings

--- utask/static/javascript/loginToggle.js
+++ utask/static/javascript/loginToggle.js
@@ -1,5 +1,14 @@
 let connexionBtn = document.getElementById('connexion-btn');
 let inscriptionBtn = document.getElementById('inscription-btn');
+let connexionForm = "<form action=\"/login\" method=\"post\">\n" +
+    "                {% csrf_token %}\n" +
+    "                <label for=\"email\" class=\"loginlbl\">Email</label><br />\n" +
+    "                <input type=\"text\" class=\"round-input\" placeholder=\"exemple@mail.com\"><br />\n" +
+    "                <label for=\"password\" class=\"passwordlbl\">Mot de passe</label><br />\n" +
+    "                <input type=\"text\" class=\"round-input\" placeholder=\"********\"><br />\n" +
+    "                <input type=\"submit\" class=\"primary-btn\" value=\"Connexion\">\n" +
+    "            </form>";
+let
 
 
 connexionBtn.addEventListener('click', function () {
