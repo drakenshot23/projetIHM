327103850e2f86d75234a54511cee308ec26bb49
Robert <robertnote23@gmail.com>
2019-04-02 09:14:27 +0200

    make settings not editable and add dev.conf for custom configuration

--- projetIHM/settings.py
+++ projetIHM/settings.py
@@ -73,7 +73,6 @@ WSGI_APPLICATION = 'projetIHM.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
---
+++ utask/static/javascript/loginToggle.js
@@ -0,0 +1,11 @@
+let connexionBtn = document.getElementById('connexion-btn');
+let inscriptionBtn = document.getElementById('inscription-btn');
+
+
+connexionBtn.addEventListener('click', function () {
+   alert('connexion');
+});
+
+inscriptionBtn.addEventListener('click', function () {
+   alert('inscription');
+});
