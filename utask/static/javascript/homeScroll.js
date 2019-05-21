93f06de03fe35bb2c2280effee277d7f6c0f9cfb
cyril.rabia <cyril.rabia@u-psud.fr>
2019-05-22 00:53:26 +0200

    modif home

--- utask/static/javascript/homeScroll.js
+++ utask/static/javascript/homeScroll.js
@@ -3,49 +3,51 @@ $(document).ready(function (){
         $('html, body').animate({
             scrollTop: $("#page_bottom").offset().top
         }, 1000);
-        $('#project').animate({
-            color : "#000"
-        },1500);
-        $('#profil').animate({
-            color : "#000"
-        },1500);
-         $('#logout').animate({
-            borderColor : "#B466A8",
-            //backgroundColor:"rgba(180, 102, 168, 0.3)",
-            color:"#B466A8"
-        },1500);
-
+        if (window.innerWidth >= 830) {
+            $('#project').animate({
+                color: "#000"
+            }, 1500);
+            $('#profil').animate({
+                color: "#000"
+            }, 1500);
+            $('#logout').animate({
+                borderColor: "#B466A8",
+                //backgroundColor:"rgba(180, 102, 168, 0.3)",
+                color: "#B466A8"
+            }, 1500);
+        }
     });
 });
 
 $(window).scroll(function () {
     var scroll = $(window).scrollTop();
-    if (scroll > 100){
-       $('#project').css({
-            'color':'black'
-        })
-        $('#profil').css({
-            'color':'black'
-        });
-        $('#logout').css({
-            'border-color':'#B466A8',
-            //'background-color':'rgba(180, 102, 168, 0.3)',
-            'color':'#B466A8'
-        });
-    }
-    else{
-        $('#project').css({
-            'color':'white'
-        });
-        $('#profil').css({
-            'color':'white'
-        });
-        $('#logout').css({
-            'border-color':'white',
-            //'background-color':'rgba(255,255,255,0.3)',
-            'color':'white'
-        });
-    }
+     if (window.innerWidth >= 830) {
+         if (scroll > 100) {
+             $('#project').css({
+                 'color': 'black'
+             })
+             $('#profil').css({
+                 'color': 'black'
+             });
+             $('#logout').css({
+                 'border-color': '#B466A8',
+                 //'background-color':'rgba(180, 102, 168, 0.3)',
+                 'color': '#B466A8'
+             });
+         } else {
+             $('#project').css({
+                 'color': 'white'
+             });
+             $('#profil').css({
+                 'color': 'white'
+             });
+             $('#logout').css({
+                 'border-color': 'white',
+                 //'background-color':'rgba(255,255,255,0.3)',
+                 'color': 'white'
+             });
+         }
+     }
 });
 
 
