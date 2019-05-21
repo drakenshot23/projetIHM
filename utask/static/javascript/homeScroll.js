3bee41e559f4cd009cb2b6a305da896dfc0a59f3
cyril.rabia <cyril.rabia@u-psud.fr>
2019-05-21 10:46:11 +0200

    update js home

--- utask/static/javascript/homeScroll.js
+++ utask/static/javascript/homeScroll.js
@@ -3,6 +3,14 @@ $(document).ready(function (){
         $('html, body').animate({
             scrollTop: $("#page_bottom").offset().top
         }, 1000);
+        $('#project').animate({
+            color : "#000"
+        },1500);
     });
 });
+
+$(window).scroll(function () {
+     
+})
+
 
